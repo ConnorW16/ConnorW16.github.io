d759a0e7752a1b1826d444c669c5e9494a04150d
Connor <waltonconnor1@gmail.com>
2024-07-07 22:32:27 +0100

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -11,8 +11,7 @@ function openProject(evt, projectName){
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    captions = document.getElementsByClassName("captions");
-    captions.innerHTML += "<div class='imageCaption'><p id='caption'></p></div>";
+    document.getElementsByClassName("captions").innerHTML += "<div class='imageCaption'><p id='caption'></p></div>";
 
     document.getElementById(projectName).style.display = "block";
     evt.currentTarget.className += " active";
