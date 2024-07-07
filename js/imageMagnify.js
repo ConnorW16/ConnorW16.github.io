7709ab06b41388c5fb5aad39ea08f6015b7c04b1
Connor <waltonconnor1@gmail.com>
2024-07-08 00:04:10 +0100

    Update imageMagnify.js

--- js/imageMagnify.js
+++ js/imageMagnify.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const slides2 = document.querySelectorAll('.slideIMG2');
     const overlay = document.getElementById('overlay');
     const overlayImage = document.getElementById('overlay-image');
+    const overlay2 = document.getElementById('overlay2');
+    const overlayImage2 = document.getElementById('overlay-image2');
     const closeBtn = document.querySelector('.close');
 
     slides.forEach(slide => {
@@ -13,8 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     slides2.forEach(slide2 => {
         slide2.addEventListener('click', () => {
-            overlayImage.src = slide.src;
-            overlay.style.display = 'flex';
+            overlayImage2.src = slide.src;
+            overlay2.style.display = 'flex';
         });
     });
 
@@ -25,6 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
     overlay.addEventListener('click', (e) => {
         if (e.target !== overlayImage) {
             overlay.style.display = 'none';
+        }
+    });
+    overlay2.addEventListener('click', (e) => {
+        if (e.target !== overlayImage2) {
+            overlay2.style.display = 'none';
         }
     });
 });
