45192cd6b3a7964fe61a001063b2b3ac68774bf8
Connor <waltonconnor1@gmail.com>
2024-07-07 23:24:45 +0100

    Update imageSlides.js

--- js/imageSlides.js
+++ js/imageSlides.js
@@ -13,6 +13,7 @@ function showSlides(n){
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("displayedImage");
     let captionText = document.getElementById("caption");
+    let captionText2 = document.getElementById("caption2");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++){
@@ -24,4 +25,5 @@ function showSlides(n){
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
+    captionText2.innerHTML = dots[slideIndex-1].alt;
 }
