1a016788e2a870c608b736c82763e56144654c7e
Connor <waltonconnor1@gmail.com>
2024-07-09 00:42:04 +0100

    Update imageSlides.js

--- js/imageSlides.js
+++ js/imageSlides.js
@@ -10,7 +10,7 @@ function currentSlide(n){
 function showSlides(n){
     let i;
     slideIndex = n;
-    let currentTab = document.querySelector('.tablinks active');
+    let currentTab = document.querySelector('.tablinks');
     let tabValue = currentTab.innerHTML;
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("displayedImage");
